11c03908e06ce900e5be7b467e9245a03264c834
Marina <markovshut@gmail.com>
2024-06-04 14:42:58 +1000

    add new  section

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ items.forEach(item => {
         })
     }
 })
+
+const back = document.querySelector("#back");
+const next = document.querySelector("#next");
+const photos = ["img/11.jpg", "img/22.jpg", "img/33.jpg", "img/44.jpg", "img/55.jpg", "img/66.jpg"];
+let i = 0;
+next.addEventListener('click', () => {
+    i++;
+    if (i > photos.length - 1) {
+        i = 0;
+    }
+    document.querySelector('#pictures').src = photos[i];
+})
+
+back.addEventListener('click', () => {
+    i--;
+    if (i < 0) {
+        i = photos.length - 1;
+    }
+    document.querySelector('#pictures').src = photos[i];
+})
